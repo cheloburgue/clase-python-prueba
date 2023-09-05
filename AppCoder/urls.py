371956596237c3445340c5f3812289b83fb5ce93
mburgue <marcelo.burgueno@uala.com.ar>
2023-09-05 09:05:00 -0300

    Update clase 22

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -11,4 +11,13 @@ urlpatterns =[
     path("entregables/",entregables, name="entregables"),
     path("busquedaComision", busquedaComision, name = "busquedaComision"),
     path("buscarDatos", buscarDatos, name="buscarDatos"),
+
+    path("eliminarProfesor/<id>", eliminarProfesor, name = "eliminarProfesor"), #Recibe un id la url que es el que va a eliminar
+    path("editarProfesor/<id>", editarProfesor, name = "editarProfesor"),
+
+    path("estudiante/list/",EstudianteList.as_view(), name= "estudiante_list"),
+    path("estudiante/nuevo/",EstudianteCreacion.as_view(), name= "estudiante_crear"),
+    path("estudiante/<pk>", EstudianteDetalle.as_view(), name = "estudiante_detalle"),
+    path("estudiante/borrar/<pk>", EstudianteDelete.as_view(), name = "estudiante_borrar"),
+    path("estudiante/editar/<pk>", EstudianteUpdate.as_view(), name = "estudiante_editar"),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .models import Curso, Profesor
+from .models import Curso, Profesor, Estudiante
 from django.http import HttpResponse
 from .forms import CursoForm, ProfesorForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 def crear_curso(request):
@@ -32,15 +34,70 @@ def profesores(request):
             info = form.cleaned_data
             profesor = Profesor(nombre=info["nombre"],apellido=info["apellido"],emails=info["email"],profesion=info["profesion"])
             profesor.save()
-            formulario_profesor = ProfesorForm()
-            return render(request, "AppCoder/profesores.html", {"mensaje": "Profesor creado","formulario":formulario_profesor})
-        return render(request, "AppCoder/profesores.html", {"mensaje": "Datos invalidos"})
+            mensaje = "Profesor creado"
+        else:
+            mensaje = "Datos invalidos"
+        formulario_profesor = ProfesorForm()
+        profesores = Profesor.objects.all()
+        return render(request, "AppCoder/profesores.html", {"mensaje": mensaje ,"formulario":formulario_profesor,"profesores":profesores})
     else: 
         #si viene por get mostrame el form vacio
         formulario_profesor = ProfesorForm()
-    #profesores = Profesor.objects.all()
-    #devuelve el form vacio en la vista hacia el template
-    return render(request, "AppCoder/profesores.html", {"formulario": formulario_profesor})
+        profesores = Profesor.objects.all()
+    #devuelve el form vacio en la vista hacia el template ,"profesores": profesores,"profesores": profesores
+    return render(request, "AppCoder/profesores.html", {"formulario": formulario_profesor, "profesores":profesores})
+
+def eliminarProfesor(request,id):
+    profesor = Profesor.objects.get(id=id)
+    profesor.delete()
+    profesores = Profesor.objects.all()
+    formulario_profesor = ProfesorForm()
+    mensaje = "Profesor Eliminado!"
+    return render(request, "AppCoder/profesores.html", {"mensaje": mensaje,"formulario": formulario_profesor,"profesores": profesores})
+
+def editarProfesor(request,id):
+    profesor = Profesor.objects.get(id=id)
+
+    if request.method == "POST":
+        form = ProfesorForm(request.POST)
+        if form.is_valid():
+            info = form.cleaned_data
+            profesor.nombre = info["nombre"]
+            profesor.apellido = info["apellido"]
+            profesor.emails = info["email"]
+            profesor.profesion = info["profesion"]
+            profesor.save()
+            mensaje = "Cambios Guardados!"
+            profesores = Profesor.objects.all()
+            formulario_profesor = ProfesorForm ()
+            return render (request, "AppCoder/profesores.html", {"mensaje":mensaje, "formulario": formulario_profesor, "profesores": profesores})
+
+    else:
+        formulario_profesor = ProfesorForm(initial={"nombre":profesor.nombre,"apellido":profesor.apellido,
+                                                    "email":profesor.emails,"profesion":profesor.profesion})
+        return render(request,"AppCoder/editarProfesor.html", {"formulario":formulario_profesor,"profesor":profesor} )
+
+class EstudianteList(ListView):
+    model = Estudiante
+    template_name = "AppCoder/estudiantes.html"
+
+class EstudianteCreacion(CreateView):
+    model = Estudiante
+    success_url = reverse_lazy("estudiante_list") # donde se dirige cuando ya este cargado el estudiante
+    fields = ["nombre","apellido","email"]
+
+class EstudianteDetalle(DetailView):
+    model = Estudiante
+    template_name = "AppCoder/estudiante_detalle.html"
+
+class EstudianteDelete(DeleteView):
+    model = Estudiante
+    success_url = reverse_lazy("estudiante_list")
+
+class EstudianteUpdate(UpdateView):
+    model = Estudiante
+    success_url = reverse_lazy("estudiante_list")
+    fields = ['nombre','apellido','email']
 
 def cursos(request):
     if request.method=="POST":
